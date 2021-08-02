49dade57e753e209975082ebe10bf68742732bd0
julien-guillan <guillan.julien@live.com>
2021-08-02 15:39:06 +0200

    Initial Commit

---
+++ GPS_speed/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GPS_speed
+//
+//  Created by Julien Guillan on 02/08/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
