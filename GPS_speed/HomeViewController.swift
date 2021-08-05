0d8774afc63b216ffb0dd8ef02ab205d9b2249ad
julien-guillan <guillan.julien@live.com>
2021-08-05 09:17:58 +0200

    1st ver

---
+++ GPS_speed/HomeViewController.swift
@@ -0,0 +1,53 @@
+//
+//  HomeViewController.swift
+//  GPS_speed
+//
+//  Created by Julien Guillan on 02/08/2021.
+//
+
+import UIKit
+import CoreLocation
+
+class HomeViewController: UIViewController {
+    
+    var speed: CLLocationSpeed = CLLocationSpeed()
+    var locationManager: CLLocationManager = CLLocationManager()
+    
+    @IBOutlet var speedLabel: UILabel!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        UIApplication.shared.isIdleTimerDisabled = true
+        print("VIEW DID LOAD")
+        locationManager.delegate = self
+        self.locationManager.requestAlwaysAuthorization()
+        // For use in foreground
+        self.locationManager.requestWhenInUseAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
+    }
+
+}
+
+extension HomeViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        self.speed = self.locationManager.location?.speed ?? 0
+        self.speed = self.speed * 3.6
+        print(self.speed)
+        self.speedLabel.text = (String(format: "%.1f Km/h", self.speed))
+    }
+    
+    private func locationManager(manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        if status != CLAuthorizationStatus.denied{
+            locationManager.startUpdatingLocation()
+        }
+    }
+}
