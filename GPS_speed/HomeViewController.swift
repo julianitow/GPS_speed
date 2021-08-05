596e535029563fb9f7943d03b6634c358f1f0c75
julien-guillan <guillan.julien@live.com>
2021-08-05 09:36:26 +0200

    Distance reset + calcul

--- GPS_speed/HomeViewController.swift
+++ GPS_speed/HomeViewController.swift
@@ -12,8 +12,11 @@ class HomeViewController: UIViewController {
     
     var speed: CLLocationSpeed = CLLocationSpeed()
     var locationManager: CLLocationManager = CLLocationManager()
+    var parcours: [CLLocation] = []
+    var distance: Double = 0
     
     @IBOutlet var speedLabel: UILabel!
+    @IBOutlet var distanceLabel: UILabel!
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
@@ -33,15 +36,25 @@ class HomeViewController: UIViewController {
         }
     }
 
+    @IBAction func onClickReset(_ sender: Any) {
+        self.distance = 0;
+        self.parcours = []
+    }
 }
 
 extension HomeViewController: CLLocationManagerDelegate {
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        self.parcours.append(locations[0])
+        if(self.parcours.count >= 2) {
+            let lastDistance = self.parcours.last?.distance(from: self.parcours[self.parcours.count - 2])
+            self.distance += lastDistance ?? 0
+        }
         self.speed = self.locationManager.location?.speed ?? 0
         self.speed = self.speed * 3.6
         print(self.speed)
         self.speedLabel.text = (String(format: "%.1f Km/h", self.speed))
+        self.distanceLabel.text = (String(format: "%.1f m", self.distance))
     }
     
     private func locationManager(manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
