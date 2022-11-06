2efdda12b273a790dae5a1e5b2b69c86f1734d98
julien guillan <guillan.julien@live.com>
2022-11-06 18:10:31 +0100

    Update for deprecated use of location locationServicesEnabled

--- GPS_speed/HomeViewController.swift
+++ GPS_speed/HomeViewController.swift
@@ -12,11 +12,13 @@ class HomeViewController: UIViewController {
     
     var speed: CLLocationSpeed = CLLocationSpeed()
     var locationManager: CLLocationManager = CLLocationManager()
-    var parcours: [CLLocation] = []
+    var trip: [CLLocation] = []
     var distance: Double = 0
     
     @IBOutlet var speedLabel: UILabel!
     @IBOutlet var distanceLabel: UILabel!
+    @IBOutlet var unitLabel: UILabel!
+    @IBOutlet var meanSpeedLabel: UILabel!
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
@@ -25,41 +27,54 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         UIApplication.shared.isIdleTimerDisabled = true
-        locationManager.delegate = self
+        self.locationManager.delegate = self
         self.locationManager.requestAlwaysAuthorization()
         // For use in foreground
         self.locationManager.requestWhenInUseAuthorization()
-        if CLLocationManager.locationServicesEnabled() {
-            locationManager.delegate = self
-            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
-            locationManager.startUpdatingLocation()
+        if self.locationManager.authorizationStatus == CLAuthorizationStatus.authorizedAlways {
+            self.locationManagerDidChangeAuthorization(self.locationManager)
+        } else {
+            self.speedLabel.sizeToFit()
+            self.speedLabel.text = "Not authorized"
         }
     }
 
     @IBAction func onClickReset(_ sender: Any) {
         self.distance = 0;
-        self.parcours = []
+        self.trip = []
     }
 }
 
 extension HomeViewController: CLLocationManagerDelegate {
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        self.parcours.append(locations[0])
-        if(self.parcours.count >= 2) {
-            let lastDistance = self.parcours.last?.distance(from: self.parcours[self.parcours.count - 2])
+        var tripText = ""
+        self.trip.append(locations[0])
+        if(self.trip.count >= 2) {
+            let lastDistance = self.trip.last?.distance(from: self.trip[self.trip.count - 2])
             self.distance += lastDistance ?? 0
         }
         self.speed = self.locationManager.location?.speed ?? 0
         self.speed = self.speed * 3.6
-        print(self.speed)
-        self.speedLabel.text = (String(format: "%.1f Km/h", self.speed))
-        self.distanceLabel.text = (String(format: "%.1f m", self.distance))
+        self.speedLabel.text = (String(format: "%.1f", self.speed))
+        if self.distance / 1000 >= 1 {
+            tripText = (String(format: "%.2f km", self.distance / 1000))
+        } else {
+            tripText = (String(format: "%.1f m", self.distance))
+        }
+        self.distanceLabel.text = tripText
     }
     
     private func locationManager(manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
         if status != CLAuthorizationStatus.denied{
             locationManager.startUpdatingLocation()
+        }
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        if manager.authorizationStatus == CLAuthorizationStatus.authorizedAlways {
+            self.locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            self.locationManager.startUpdatingLocation()
         }
     }
 }
